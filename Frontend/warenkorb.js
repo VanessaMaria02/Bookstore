3e6e83b03e93edf9ca82c7471900816b3d1bdb91
VanessaMaria02 <wi21b004@technikum-wien.at>
2023-06-12 20:33:48 +0200

    Warenkorb fertig
    Man kann jetzt auch Produkte löschen und die Anzahl ändern.

--- Frontend/warenkorb.js
+++ Frontend/warenkorb.js
@@ -1,22 +1,57 @@
 $(document).ready(function() {
     console.log(document.cookie);
     displayprodukte();
+    $(document).on("change", "#change", changeAnzahl);
+    $(document).on("click", "#remove", removeItem);
 
 });
 
-function setCookie(product){
-
+function changeAnzahl(){
+    var productId = this.parentNode.id;
+    var anzahl = this.value;
     var cartProducts = getCardProducts();
+    var newProducts =[];
+    var product;
+    cartProducts.forEach(element =>{
+        if(element[0].id != productId){
+            newProducts.push(element);
+        } else {
+            product = element;
+        }
+    });
 
-    cartProducts.push(product);
-
-    
-
-    setCartProducts(cartProducts);
-
-    console.log(document.cookie);
-
+    for(var i = 0; i < anzahl; i++){
+        newProducts.push(product);
+        console.log(i);
+    };
+    setCartProducts(newProducts);
+    location.reload();
 }
+
+function removeItem(){
+    var productId = this.parentNode.id;
+    
+    var cartProducts = getCardProducts();
+    console.log(cartProducts);
+    var newProducts =[];
+    cartProducts.forEach(element =>{
+        if(element[0].id != productId){
+            newProducts.push(element);
+        }
+    });
+    console.log(newProducts);
+    setCartProducts(newProducts);
+    location.reload();
+}
+
+
+function setCartProducts(products){
+    var productsJSON = JSON.stringify(products);
+    document.cookie = "cartProducts=" + productsJSON +"; path=/"
+}
+
+
+
 
 function getCardProducts(){
     var cookieValue = document.cookie
@@ -35,14 +70,20 @@ function getCardProducts(){
 
 function displayprodukte(){
     var cart = getCardProducts();
-    var counter = 0;
+
+    if(cart.length === 0){
+        $("#produkteWarenkorb").append('<p>Es sind keine Produkte im Warenkorb</p>'); 
+    }
 
     var productCounts ={};
 
     var uniqueProducts =[];
 
+    console.log(cart);
+
     for(var i = 0; i < cart.length; i++){
         var innerArray = cart[i];
+        console.log(innerArray);
 
         if(innerArray.length >0){
             var product = innerArray[0];
@@ -57,18 +98,29 @@ function displayprodukte(){
         }
     }
 
-    for(var i = 0; i < uniqueProducts.length; i++){
-        counter++;
-        var innerArray2 = cart[i];
+    console.log(uniqueProducts);
+    console.log(productCounts);
 
+    for(var i = 0; i < uniqueProducts.length; i++){
+        var innerArray2 = uniqueProducts[i];
+        console.log(innerArray2);
         if(innerArray2.length >0){
             var product = innerArray2[0];
             var productpreis = product.preis*productCounts[product.id];
-            $("#produkteWarenkorb").append('<p><img with = 80px height = 130px src = "./res/img/produktBilder/'+product.image_url+'"><b>'+product.name+'</b> Anzahl: '+productCounts[product.id]+' Preis: '+productpreis+'€</p>');
+            $("#produkteWarenkorb").append('<p id="'+product.id+'"><img with = 80px height = 130px src = "./res/img/produktBilder/'
+            +product.image_url
+            +'"><b>'
+            +product.name
+            +'</b> Anzahl: <input type = "number" id="change" value="'
+            +productCounts[product.id]
+            +'"> Preis: '
+            +productpreis
+            +'€'+ ' <button id = "remove">X</button>'
+            +'</p>' 
+            );
         }
     }
 
-    
         
        
     
